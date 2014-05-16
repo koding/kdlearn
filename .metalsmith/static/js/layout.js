b558e50c8121917cdedeb1bbd16d9d6552334ebc
Stefan Cosma <stefcosma@gmail.com>
2014-05-16 18:44:49 +0000

    Added fancybox to videos

--- .metalsmith/static/js/layout.js
+++ .metalsmith/static/js/layout.js
@@ -83,4 +83,16 @@ $(document).ready(function () {
     });
 
     prettyPrint();
+    
+    $(".youtube").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'elastic',
+		closeEffect	: 'none'
+	});
 });
