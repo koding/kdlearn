08fc8121bda93cfe53abfd6b142760bc76520692
Lee Olayvar <leeolayvar@gmail.com>
2015-03-26 23:35:20 +0000

    Manually updated kpm.sh script

--- kpm.sh
+++ kpm.sh
@@ -3,43 +3,42 @@
 # # Koding Package Manager Installer
 #
 # The following script is a shorthand script, used as a frontend
-# for the actual installer (located in ./installer). This script
+# for the actual installer (located in ./os/kpm/install). This script
 # is **not** run by KPM at all.
 #
-
 main()
 {
-ubuntu_required_msg()
-{
-  echo ""
-  echo "KPM currently depends on Ubuntu, and only runs on Koding VMs."
-  echo "Please retry this on a standard Ubuntu Koding VM."
-  echo ""
-  exit 1
-}
+# ## Get the platform
+platform=`uname | tr '[:upper:]' '[:lower:]'`
+os=""
+case "$platform" in
+  darwin)
+    os="osx"
+    ;;
+  linux)
+    # The only supported linux distro currently
+    os="ubuntu"
+    ;;
+  windows)
+    os="$platform"
+    ;;
+  *)
+    cat << EOF
+Error 84: Unknown platform.
+EOF
+  exit 84
+  ;;
+esac
 
-OS=$(lsb_release -si)
-if [ "$?" -ne "0" ]; then
-  ubuntu_required_msg
-elif [ "$OS" != "Ubuntu" ]; then
-  ubuntu_required_msg
+
+branch='master'
+if [ "$KPM_DEVELOP" = "true" ]; then
+  branch='develop'
 fi
 
-which fish > /dev/null
-if [ "$?" -ne "0" ]; then
-  echo ""
-  echo "Error: Fish not found"
-  echo ""
-  echo "The Fish shell is currently required by KPM to run properly."
-  echo "Please install Fish with the following command, and retry this"
-  echo "installer:"
-  echo ""
-  echo "    \$ sudo apt-get install fish"
-  echo ""
-  exit 1
-fi
 
-curl -sSL https://raw.githubusercontent.com/koding/kpm-scripts/master/kpm/installer | fish
+# ## Pipe the actual installer
+curl -sSL "https://raw.githubusercontent.com/koding/kpm-scripts/$branch/$os/kpm/install" | bash
 exit $?
 }
 main
